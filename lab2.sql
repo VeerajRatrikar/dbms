8514dc1b920ba8fa41a8f5896ac19488152828e4
Veeraj Sadashiva Ratrikar <145032949+VeerajRatrikar@users.noreply.github.com>
2025-03-06 10:40:23 +0530

    Update lab2.sql

--- lab2.sql
+++ lab2.sql
@@ -26,8 +26,24 @@ Salesman_ID references Salesman(Salesman_ID) on delete set null
 insert into Orders values(&Ord_No,'&Purchase','&Ord_Date',&Customer_ID,&Salesman_ID);
 
 Questions:
-1)Select grade,count(cust_id) from your Customer group by having grade>(select avg(grade)) from customer where city='Banglore';
-2)Select salesman_id,name from salesman A where 1<(select count(*) from customer C where  C.salesman_id = A.salesman_id);
-3)Select s.salesman_id,name,customer,commission from salesman s , customer C where s.city=C.city
-    UNION   
-  Select s.salesman,no_math,customer,commission from Salesman s,Customer C where Not city = any(select city from )
+1)Select Grade,count(Customer_ID) from Customer group by Grade having Grade>(select avg(Grade) from Customer where City='Banglore');
+2)Select Salesman_ID,Name from Salesman A where 1<(select count(*) from Customer C where  C.Salesman_ID = A.Salesman_ID);
+3)Select S.Salesman_ID,Name,Customer_ID,Commision from Salesman S , Customer C where S.City=C.City
+  UNION   
+  Select S.Salesman,S.Name,NO_MATCH,Customer_ID,Commision from Salesman S,Customer C where Not City = any(select City from Customer);
+4)Create view E_Salesman as Select B.Ord_date,A.Salesman_ID,A.Name from Salesman A,Orders B where A.Salesman_ID=B.Salesman_ID and
+  B.Purchase_amt=(Select max(Purchase_amt)from Orders C where C.Ord_date = B.Ord_date);
+5)delete from Salesman where Salesman_ID=1000;
+
+
+3)SELECT S.Salesman_ID, S.Name, C.Customer_ID, S.CommisioN FROM Salesman S, Customer C WHERE S.City = C.City UNION
+SELECT S.Salesman_ID, S.Name, NULL AS Customer_ID, S.Commision FROM Salesman S WHERE NOT EXISTS (SELECT 1 FROM Customer C WHERE C.City = S.City);
+
+ SELECT * FROM E_Salesman;
+ORD_DATE  SALESMAN_ID NAME
+--------- ----------- ------------------
+04-MAY-17        1000 John
+20-JAN-17        2000 Ravi
+13-APR-17        3000 Kumar
+09-MAR-17        2000 Ravi
+
