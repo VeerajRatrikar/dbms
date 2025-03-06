6b3ccd69468ef9a907587018fe7920d86cf95aaf
Veeraj Sadashiva Ratrikar <145032949+VeerajRatrikar@users.noreply.github.com>
2025-03-06 09:16:10 +0530

    Update lab2.sql

--- lab2.sql
+++ lab2.sql
@@ -6,3 +6,12 @@ create table Salesman(
 );
 
 insert into Salesman values(&Salesman_ID,'&Name','&City','&Commission');
+
+create table Customer(Customer_ID int primary key,
+Cust_Name varchar(20),
+City varchar(20),
+Grade int,
+Salesman_ID references Salesman(Salesman_ID) on delete set null
+);
+
+insert into Customer values(&Customer_ID,'&Cust_Name','&City',&Grade,&Salesman_ID);
