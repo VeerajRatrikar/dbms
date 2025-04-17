309e4604f1f54f2b5ccfb4740ee38d21483055d5
Veeraj Sadashiva Ratrikar <145032949+VeerajRatrikar@users.noreply.github.com>
2025-04-17 09:12:28 +0530

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -44,3 +44,33 @@ e) For each department that has more than five employees, retrieve the departmen
 select d.dno,d.dname,e.fname,e.lname ,count(*) as noofemp from Emp e, Depts d where
 e.dno=d.dno and e.salary>600000 and d.dno in(select e1.dno from Emp e1 group by e1.dno
 having count(*)>5) group by d.dno,d.dname,e.fname,e.lname;
+
+
+SELECT DISTINCT P.PNO FROM PRJ P, DEPTS D, EMP E WHERE E.DNO = D.DNO AND D.DNO = P.DNO AND (E.LNAME = 'SCOTT' OR D.MGRSSN IN (SELECT SSN FROM EMP WHERE LNAME = 'SCOTT'));
+     PNO
+--------
+      60
+      10
+SELECT E.SSN, E.FNAME, E.LNAME, 1.1*E.SALARY AS RAISEDSAL FROM EMP E, PRJ P, WORK W WHERE P.PNAME = 'IOT' AND P.PNO = W.PNO AND E.SSN = W.SSN;
+
+    SSN FNAME                LNAME                 RAISEDSAL
+------- -------------------- -------------------- ----------
+    111 JAMES                SCOTT                  135801.6
+    222 RAMESH               ARAVIND                380239.2
+    333 DAVID                HUSH                      77000
+SELECT SUM(SALARY) AS SUMSAL, AVG(SALARY) AS AVGSAL, MIN(SALARY) AS MINSAL, MAX(SALARY) AS MAXSAL FROM EMP E, DEPTS D WHERE E.DNO = D.DNO AND D.DNAME = 'ACCOUNTS';
+   SUMSAL     AVGSAL     MINSAL     MAXSAL
+--------- ---------- ---------- ----------
+  2299128     383188      70000     750000
+SELECT E.FNAME, E.LNAME FROM EMP E WHERE NOT EXISTS ((SELECT PNO FROM PRJ WHERE DNO = 5) MINUS (SELECT PNO FROM WORK WHERE E.SSN = SSN));
+FNAME                LNAME
+-------------------- ---------------
+JAMES                SCOTT
+RAMESH               ARAVIND
+DAVID                HUSH
+
+SELECT D.DNO, D.DNAME, COUNT(*) AS NOOFEMP FROM EMP E, DEPTS D WHERE E.DNO = D.DNO AND E.SALARY > 600000 AND 
+D.DNO IN (SELECT E1.DNO FROM EMP E1 GROUP BY E1.DNO HAVING COUNT(*) > 5) GROUP BY D.DNO, D.DNAME;
+     DNO DNAME                   NOOFEMP
+-------- -------------------- ----------
+       1 ACCOUNTS                      1
